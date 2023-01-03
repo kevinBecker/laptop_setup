5e155ad91d05473ece78bf40d9ae8c9780483eb1
Kevin Becker <kjbecker00@gmail.com>
2023-01-03 15:26:56 -0500

    Added vscode default settings

--- reload.zsh
+++ reload.zsh
@@ -2,8 +2,10 @@
 "cp" .vimrc ~/.vimrc
 "cp" .zshrc ~/.zshrc
 "cp" .pablo_names ~/.pablo_names
+"cp" vscode_settings.json Library/Application\ Support/Code/User/settings.json
 
-echo "vimrc, zshrc, pablo_names have been reloaded"
+
+echo "vimrc, zshrc, pablo_names have been reloaded. vscode default json settings updated"
 
 
 echo "Remember to re-source ~/.zshrc"
