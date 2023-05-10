8f8e5c835c79b8d4d267bf419b35773e5833aea9
kjbecker00 <kevin00@mit.edu>
2023-05-10 08:51:18 -0400

    Added launch.sh, recursivebuild, updated to tg, added hg

--- reminders.sh
+++ reminders.sh
@@ -22,8 +22,11 @@ printf "\e[38;2;%s;%s;%sm\e[2m on shell scripts\n\e[0m" "$R3" "$G3" "$B3"
 printf "\e[38;2;%s;%s;%sm\t- pyflakes" "$R2" "$G2" "$B2"
 printf "\e[38;2;%s;%s;%sm\e[2m on python script debugging\n\e[0m" "$R3" "$G3" "$B3"
 
-printf "\e[38;2;%s;%s;%sm\t- tf\e[0m" "$R2" "$G2" "$B2"
+printf "\e[38;2;%s;%s;%sm\t- tg\e[0m" "$R2" "$G2" "$B2"
 printf "\e[38;2;%s;%s;%sm\e[2m alias for tree -f | grep ____\n\e[0m" "$R3" "$G3" "$B3"
+
+printf "\e[38;2;%s;%s;%sm\t- hg\e[0m" "$R2" "$G2" "$B2"
+printf "\e[38;2;%s;%s;%sm\e[2m alias for history | grep ____\n\e[0m" "$R3" "$G3" "$B3"
 
 printf "\e[38;2;%s;%s;%sm\t- mcd\e[0m" "$R2" "$G2" "$B2"
 printf "\e[38;2;%s;%s;%sm\e[2m function to make a dir and enter it\n\e[0m" "$R3" "$G3" "$B3"
@@ -33,5 +36,11 @@ printf "\e[38;2;%s;%s;%sm\e[2m alias for source ~/.zshrc\n\e[0m" "$R3" "$G3" "$B
 
 printf "\e[38;2;%s;%s;%sm\t- rm \e[0m" "$R2" "$G2" "$B2"
 printf "\e[38;2;%s;%s;%sm\e[2m alias for trash\n\e[0m" "$R3" "$G3" "$B3"
+
+
+printf "\e[38;2;%s;%s;%sm\t- rb \e[0m" "$R2" "$G2" "$B2"
+printf "\e[38;2;%s;%s;%sm\e[2m function for recursive build (goes back until it finds build.sh, runs -j8)\n\e[0m" "$R3" "$G3" "$B3"
+
+
 
 printf "\n"
