91420e0f60c7e720f0e2d0fb19e70a959a6b340c
kjbecker00 <kevin00@mit.edu>
2024-01-17 12:42:39 -0500

    Update vim reminders

--- vim_reminders.sh
+++ vim_reminders.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
-R=80
-G=255
-B=80
+R1=80
+G1=255
+B1=80
 
 R2=0
 G2=255
@@ -12,34 +11,55 @@ R3=$R2
 G3=$G3
 B3=$B3
 
+
+R4=80
+G4=150
+B4=168
+bold_cecho() {
+    var="R$2"
+    R=$(eval echo \$$var)
+    var="G$2"
+    G=$(eval echo \$$var)
+    var="B$2"
+    B=$(eval echo \$$var)
+    printf "\e[38;2;%s;%s;%sm\e[1m\n$1\e[0m" "$R" "$G" "$B"
+}
+cecho() {
+    var="R$2"
+    R=$(eval echo \$$var)
+    var="G$2"
+    G=$(eval echo \$$var)
+    var="B$2"
+    B=$(eval echo \$$var)
+    printf "\e[38;2;%s;%s;%sm$1" "$R" "$G" "$B"
+}
+
+
+
 # ---------------------------------------------------
-printf "\e[38;2;%s;%s;%sm\e[1m\nFriendly reminders of vim shortcuts (from vimtutor):\n\e[0m" "$R" "$G" "$B"
-# ---------------------------------------------------
+bold_cecho "Reminders of vim shortcuts:\n"  1
 
-printf "\e[38;2;%s;%s;%sm\t- Move to w,e,$,0" "$R2" "$G2" "$B2"
-printf "\e[38;2;%s;%s;%sm\e[2m next word, end of word, eol, bol)\n\e[0m" "$R3" "$G3" "$B3"
-
-printf "\e[38;2;%s;%s;%sm\t- d2w, de, d$ dd" "$R2" "$G2" "$B2"
-printf "\e[38;2;%s;%s;%sm\e[2m cut two words, delete upto next word, delete to eol, delete line\n\e[0m" "$R3" "$G3" "$B3"
-
-printf "\e[38;2;%s;%s;%sm\t- p" "$R2" "$G2" "$B2"
-printf "\e[38;2;%s;%s;%sm\e[2m Paste (from last deleted with d__)\n\e[0m" "$R3" "$G3" "$B3"
-
-printf "\e[38;2;%s;%s;%sm\t- u, U, CTRL-R\e[0m" "$R2" "$G2" "$B2"
-printf "\e[38;2;%s;%s;%sm\e[2m undo last, restore current line, redo -f | grep ____\n\e[0m" "$R3" "$G3" "$B3"
-
-printf "\e[38;2;%s;%s;%sm\t- x\e[0m" "$R2" "$G2" "$B2"
-printf "\e[38;2;%s;%s;%sm\e[2m Delete character\n\e[0m" "$R3" "$G3" "$B3"
-
-printf "\e[38;2;%s;%s;%sm\t- r_\e[0m" "$R2" "$G2" "$B2"
-printf "\e[38;2;%s;%s;%sm\e[2m replace character with _ \n\e[0m" "$R3" "$G3" "$B3"
-
-
-printf "\e[38;2;%s;%s;%sm\t- CTRL+V, select lines, SHIFT+I #\e[0m" "$R2" "$G2" "$B2"
-printf "\e[38;2;%s;%s;%sm\e[2m Comment out selected lines (visual block mode -> insert mode) \n\e[0m" "$R3" "$G3" "$B3"
-
-
-
+cecho "\t- Move to w,e,$,0 " 2
+cecho "\t- next word, end of word, eol, bol" 3
+echo ""
+cecho "\t- d2w, de, d$ dd" 2
+cecho "\t- cut two words, delete upto next word, delete to eol, delete line" 3
+echo ""
+cecho "\t- p" 2
+cecho "\t- Paste (from last deleted with d__)" 3
+echo ""
+cecho "\t- u, U, CTRL-R" 2
+cecho "\t- undo last, restore current line, redo -f | grep ____" 3
+echo ""
+cecho "\t- x" 2
+cecho "\t- Delete character" 3
+echo ""
+cecho "\t- r_" 2
+cecho "\t- Replace character with _" 3
+echo ""
+cecho "\t- CTRL+V, select lines, SHIFT+I #" 2
+cecho "\t- Comment out selected lines (visual block mode -> insert mode) " 3
+echo ""
 
 
 printf "\n"
